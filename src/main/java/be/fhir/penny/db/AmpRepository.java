ee295c766983e6746a5122487c7c5b346c7d57fb
jens <jens.penny@gmail.com>
2023-05-05 23:34:31 +0200

    Added black triangle Added basic search based on amp code

--- src/main/java/be/fhir/penny/db/AmpRepository.java
+++ src/main/java/be/fhir/penny/db/AmpRepository.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
 
 /**
  * todo: log timings
@@ -21,16 +23,15 @@ public final class AmpRepository {
         this.provider = provider;
     }
 
-    public Collection<AMP> getAmpsByName(@NotNull final String name) {
-        Collection<AMP> ampsByName = new ArrayList<>();
+    public Collection<AMP_FAHMP> getAmpsByName(@NotNull final String name) {
+        Collection<AMP_FAHMP> ampsByName = new ArrayList<>();
         try (Statement statement = provider.getConnection().createStatement()) {
             //todo input sanitation pls
             ResultSet result = statement.executeQuery("Select * from AMP_FAMHP " +
                     "where officialName like '%" + name + "%' " +      //Check if name contains - needs more info later
                     "and ifnull(validTo, date('now')) >= date('now')"); //Only valid amps
             while (result.next()) {
-                AMP amp = new AMP(result.getString("code"),
-                        result.getString("officialName"));
+                AMP_FAHMP amp = ampFromResult(result);
                 ampsByName.add(amp);
             }
         } catch (SQLException e) {
@@ -40,6 +41,63 @@ public final class AmpRepository {
         return ampsByName;
     }
 
-    public record AMP(String ampCode, String officialName) {
+    @NotNull
+    private static AMP_FAHMP ampFromResult(ResultSet result) throws SQLException {
+        return new AMP_FAHMP(
+                result.getString("code"),
+                result.getString("officialName"),
+                result.getBoolean("blackTriangle"),
+                result.getInt("companyActorNumber"),
+                result.getString("medicineType"),
+                result.getString("nameEng"),
+                result.getString("nameFr"),
+                result.getString("nameGer"),
+                result.getString("nameNl"),
+                result.getString("prescriptionNameEng"),
+                result.getString("prescriptionNameFr"),
+                result.getString("prescriptionNameGer"),
+                result.getString("prescriptionNameNl"),
+                result.getString("status"),
+                result.getDate("validFrom"),
+                result.getDate("validTo"),
+                result.getInt("vmpCode")
+        );
+    }
+
+    public Optional<AMP_FAHMP> getCurrentAmpById(@NotNull final String ampCode) {
+        try (Statement statement = provider.getConnection().createStatement()) {
+            //todo input sanitation pls
+            ResultSet result = statement.executeQuery("Select * from AMP_FAMHP " +
+                    "where code = '" + ampCode + "' " +
+                    "and ifnull(validTo, date('now')) >= date('now')"); //Only valid amps
+            while (result.next()) {
+                AMP_FAHMP amp = ampFromResult(result);
+                return Optional.of(amp);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+
+    public record AMP_FAHMP(String ampCode,
+                            String officialName,
+                            boolean blacktriangle,
+                            Integer companyActorNumber,
+                            String medicineType,
+                            String nameEng,
+                            String nameFr,
+                            String nameGer,
+                            String nameNl,
+                            String prescriptionNameEng,
+                            String prescriptionNameFr,
+                            String prescriptionNameGer,
+                            String prescriptionNameNl,
+                            String status,
+                            Date validFrom,
+                            Date validTo,
+                            Integer vmpCode
+                      ) {
     }
 }
--- src/main/java/be/fhir/penny/provider/MedicinalProductDefinitionProvider.java
+++ src/main/java/be/fhir/penny/provider/MedicinalProductDefinitionProvider.java
@@ -7,15 +7,17 @@ import ca.uhn.fhir.rest.annotation.Read;
 import ca.uhn.fhir.rest.annotation.RequiredParam;
 import ca.uhn.fhir.rest.annotation.Search;
 import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.rest.server.exceptions.NotImplementedOperationException;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.StringType;
-import org.hl7.fhir.r5.model.MedicinalProductDefinition;
+import org.hl7.fhir.r5.model.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -42,11 +44,14 @@ public class MedicinalProductDefinitionProvider implements IResourceProvider {
     @Read
     public Samv2MedicinalProductDefinition readMedicinalProductDefinition(@IdParam IdType samId) {
         if (!samId.hasVersionIdPart()) {
-            //Return latest
+            Optional<AmpRepository.AMP_FAHMP> currentAmpById = repository.getCurrentAmpById(samId.getIdPart());
+            if (currentAmpById.isEmpty()) {
+                throw new ResourceNotFoundException("Unknown current medicinal product definition: " + samId);
+            }
+            return ampToFhirMedProduct().apply(currentAmpById.get());
+        } else {
+            throw new NotImplementedOperationException("Versioned results for this operation are not implemented yet");
         }
-        //todo next
-
-        throw new ResourceNotFoundException("Unknown medicinal product definition: " + samId.toString());
     }
 
     /**
@@ -57,7 +62,7 @@ public class MedicinalProductDefinitionProvider implements IResourceProvider {
      */
     @Search()
     public List<Samv2MedicinalProductDefinition> findMedicinalProductsByName(@RequiredParam(name = Samv2MedicinalProductDefinition.SP_NAME) StringType ampName) {
-        Collection<AmpRepository.AMP> ampsByName = repository.getAmpsByName(ampName.getValue());
+        Collection<AmpRepository.AMP_FAHMP> ampsByName = repository.getAmpsByName(ampName.getValue());
         List<Samv2MedicinalProductDefinition> definitions = ampsByName.stream()
                 .map(ampToFhirMedProduct())
                 .collect(Collectors.toList());
@@ -67,13 +72,41 @@ public class MedicinalProductDefinitionProvider implements IResourceProvider {
     }
 
     @NotNull
-    private static Function<AmpRepository.AMP, Samv2MedicinalProductDefinition> ampToFhirMedProduct() {
+    private static Function<AmpRepository.AMP_FAHMP, Samv2MedicinalProductDefinition> ampToFhirMedProduct() {
         return amp -> {
             Samv2MedicinalProductDefinition def = new Samv2MedicinalProductDefinition();
             def.setId(amp.ampCode());
             MedicinalProductDefinition.MedicinalProductDefinitionNameComponent name = new MedicinalProductDefinition.MedicinalProductDefinitionNameComponent(amp.officialName());
             def.setName(Collections.singletonList(name));
+
+            //Add the other names if applicable
+            //if (amp.prescriptionNameNl() != null) {
+                //MedicinalProductDefinition.MedicinalProductDefinitionNameComponent prescriptionNl = new MedicinalProductDefinition.MedicinalProductDefinitionNameComponent();
+                //prescriptionNl.a
+            //}
+
+            if (amp.blacktriangle()) {
+                def.setAdditionalMonitoringIndicator(new CodeableConcept(new Coding("http://hl7.org/fhir/medicinal-product-additional-monitoring" , "BlackTriangleMonitoring", "Requirement for Black Triangle Monitoring")));
+            }
+
+            //See discussion in 3.MedicinalProductDefinition-first-mapping-notes.md#status
+            MarketingStatus marketingStatus = new MarketingStatus();
+            marketingStatus.setStatus(new CodeableConcept(new Coding("samv2_amp_status", amp.status(), amp.status()))); //The docs say: This attribute provides information on the status of the marketing of the medicinal product See ISO/TS 20443 for more information and examples, but I don't have free access to those
+            def.addMarketingStatus(marketingStatus);
+
             return def;
         };
+    }
+
+    private static MedicinalProductDefinition.MedicinalProductDefinitionNameComponent createName(String name, String country, String language) {
+        MedicinalProductDefinition.MedicinalProductDefinitionNameComponent component = new MedicinalProductDefinition.MedicinalProductDefinitionNameComponent(name);
+
+        if (country != null && language != null) {
+            MedicinalProductDefinition.MedicinalProductDefinitionNameUsageComponent usage = new MedicinalProductDefinition.MedicinalProductDefinitionNameUsageComponent();
+            //usage.setCountry()
+            component.addUsage(usage);
+        }
+
+        return component;
     }
 }
